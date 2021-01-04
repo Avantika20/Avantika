f1fd13f6548d96aec7d333d2d24c4d3ca4d2a1a0
Avantika20 <76730944+Avantika20@users.noreply.github.com>
2021-01-04 16:07:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function Book(){
+  window.alert("These Books Were Written By J.K. Rowling. About JK.")
+ window.alert("J.K. Rowling, in full Joanne Kathleen Rowling, (born July 31, 1965, Yate, near Bristol, England), British author, creator of the popular and critically acclaimed Harry Potter series, about a young sorcerer in training");
+}
